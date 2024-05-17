473cb18e20a468b5ac0a615cf4436c5565d33c15
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-05-17 16:20:07 +0700

    first commit

---
+++ src/main/java/com/pbomodul5/Dosen.java
@@ -0,0 +1,22 @@
+package com.pbomodul5;
+
+public class Dosen extends Person{
+    private String idDosen;
+    public Dosen(String Nama, String Alamat, int Umur, String idDosen){
+        super(Nama, Alamat, Umur);
+        this.idDosen = idDosen;
+    }
+    public String getIdDosen(){
+        return idDosen;
+    }
+    public void setIdDosen(String idDosen){
+        this.idDosen = idDosen;
+    }
+    public void infoDosen(){
+        System.out.println("\nInfoDosen");
+        System.out.println("IdDosen :\t\t "+getIdDosen());
+        System.out.println("Nama\t\t : "+getNama());
+        System.out.println("Alamat\t\t : "+getAlamat());
+        System.out.println("Umur :\t\t "+getUmur());
+    }
+}
