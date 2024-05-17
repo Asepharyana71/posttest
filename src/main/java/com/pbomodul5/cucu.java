52cb15077b763a406a2f745c83686a6cfca6c97f
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-05-17 16:50:19 +0700

    p

--- src/main/java/com/pbomodul5/cucu.java
+++ src/main/java/com/pbomodul5/cucu.java
@@ -1,5 +1,16 @@
 package com.pbomodul5;
 
-public class cucu {
-    System.out.println("Nama Kakek : "+namakakek);
+public class cucu extends Bapak {
+    public cucu() {
+        System.out.println("Nama Kakek : " + namakakek);
+        System.out.println("Alamat Kakek : " + address);
+    }
+    public static void main(String[] args) {
+        String namacucu ;
+        cucu lagan=new cucu();
+        System.out.println("Masukan adalah cucu");
+        System.out.println("Nama saya :Rio Andriyat krismawan");
+        System.out.println("");
+    }
+
 }
