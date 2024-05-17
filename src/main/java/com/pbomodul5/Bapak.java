491e05121dd196de016b75b59918ee8b0d326997
MythEclipse <90584806+Asepharyana71@users.noreply.github.com>
2024-05-17 16:46:31 +0700

    p

---
+++ src/main/java/com/pbomodul5/Bapak.java
@@ -0,0 +1,28 @@
+package com.pbomodul5;
+
+public class Bapak extends Kakek{
+    protected String namebapak;
+    protected String addressbapak;
+    public bapak(){
+    System.out.println("Nama Kakek : "+namakakek);
+    System.out.println("Alamat Kakek : "+address);
+    System.out.println("\n");
+    System.out.println("Masukan konstruktor bapak");
+    System.out.println("Dijalankan oleh class cucu");
+    namebapak="ROHAEDI"
+    addressbapak="Kuningan"
+    }
+    public bapak(String namebapak, String addressbapak){
+        this.namebapak = namebapak;
+        this.addressbapak = addressbapak;
+    }
+    public String getName(){
+        return namebapak;
+    }
+    public String getAddress(){
+        return addressbapak;
+    }
+    public static void main(String[] args) {
+        bapak sari=new bapak();
+    }
+}
---
+++ src/main/java/com/pbomodul5/cucu.java
@@ -0,0 +1,5 @@
+package com.pbomodul5;
+
+public class cucu {
+    System.out.println("Nama Kakek : "+namakakek);
+}
